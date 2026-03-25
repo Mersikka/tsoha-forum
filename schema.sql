98a4997abe70068e92c82b8a91aa117714162382
mersikka <mio@argillander.me>
2026-03-25 18:51:42 +0200

    Add votes and comments to schema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,27 @@
+CREATE TABLE assets (
+    id INTEGER PRIMARY KEY,
+    filename TEXT NOT NULL,
+    content BLOB NOT NULL
+);
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
-    username TEXT UNIQUE,
-    password_hash TEXT
+    username TEXT UNIQUE NOT NULL,
+    profile_picture INTEGER REFERENCES assets(id),
+    password_hash TEXT NOT NULL
 );
 
 CREATE TABLE threads (
     id INTEGER PRIMARY KEY,
-    title TEXT,
+    title TEXT NOT NULL,
+    body TEXT,
+    asset_id INTEGER REFERENCES assets(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY,
     body TEXT,
     user_id INTEGER REFERENCES users,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
@@ -22,3 +37,17 @@ CREATE TABLE thread_tags (
     tag_id INTEGER REFERENCES tags(id),
     PRIMARY KEY (thread_id, tag_id)
 );
+
+CREATE TABLE votes (
+    id INTEGER PRIMARY KEY,
+    voter_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    thread_id INTEGER REFERENCES threads(id) ON DELETE CASCADE,
+    comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
+    CHECK (
+        (thread_id IS NOT NULL AND comment_id IS NULL) OR
+        (thread_id IS NULL AND comment_id IS NOT NULL)
+    ),
+    UNIQUE(voter_id, thread_id),
+    UNIQUE(voter_id, comment_id)
+);
+
